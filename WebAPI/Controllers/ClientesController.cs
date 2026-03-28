ddf40ee03cb01b1539eb5d01f58443ce0f740cd5
luis <luis.g.mendonca@aluno.senai.br>
2026-03-28 13:26:07 -0300

    completo

--- WebAPI/Controllers/ClientesController.cs
+++ WebAPI/Controllers/ClientesController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -13,6 +14,7 @@ namespace WebAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class ClientesController : ControllerBase
     {
         private readonly ApplicationDbContext _context;
